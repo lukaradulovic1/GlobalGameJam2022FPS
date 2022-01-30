2165be87399a171ef2440f7f74ccd03d30e27d23
Djordje Cvijic <djordjec@nordeus.com>
2022-01-30 14:26:31 +0100

    Fix shooting

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -1,35 +1,30 @@
 using StarterAssets;
 using UnityEngine;
 
-
 public class Gun : MonoBehaviour
 {
-    public GameObject pewPewPrefab;
-    public float damage = 10f;
-    public float range = 100f;
-    public StarterAssetsInputs inputs;
+	public GameObject pewPewPrefab;
+	public float damage = 10f;
+	public float range = 100f;
+	public StarterAssetsInputs inputs;
+	public Camera fpsCam;
 
-    public Camera fpsCam;
+	// Update is called once per frame
+	private void Update()
+	{
+		if (inputs.shoot) Shoot();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (inputs.shoot)
-        {
-            Shoot();
-        }
-    }
-    void Shoot()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
-        {
-            Debug.Log(hit.transform.name);
-            var target = hit.transform.GetComponent<Target>();
-            if (target != null)
-            {
-                target.TakeDamage(10);
-            }
-        }
-    }
+	private void Shoot()
+	{
+		if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out var hit, range))
+		{
+			var target = hit.transform.GetComponent<Target>();
+			if (target != null)
+			{
+				target.TakeDamage(10);
+				if (!target.IsDead) Debug.Log(hit.transform.name);
+			}
+		}
+	}
 }
