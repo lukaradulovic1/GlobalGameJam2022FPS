faeac4902b6dc4b7d378dbfd4c6f700e73f53380
lukaradulovic1 <luka.radulovic.011@gmail.com>
2022-01-31 23:54:58 +0100

    added gun sounds

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -3,37 +3,46 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-	public GameObject pewPewPrefab;
-	public Transform pewPewHolder;
-	public float damage = 10f;
-	public float range = 100f;
-	public StarterAssetsInputs inputs;
-	public Camera fpsCam;
-	public float shotsPerSecond = 10f;
+    public GameObject pewPewPrefab;
+    public Transform pewPewHolder;
+    public float damage = 10f;
+    public float range = 100f;
+    public StarterAssetsInputs inputs;
+    public Camera fpsCam;
+    public float shotsPerSecond = 10f;
 
-	private float shotCooldownProgress;
+    private AudioSource shootingSound;
 
-	// Update is called once per frame
-	private void FixedUpdate()
-	{
-		shotCooldownProgress += Time.deltaTime * shotsPerSecond;
-		if (inputs.shoot) Shoot();
-	}
+    public void Start()
+    {
+        shootingSound = GetComponent<AudioSource>();
+    }
 
-	private void Shoot()
-	{
-		if (shotCooldownProgress < 1f) return;
+    private float shotCooldownProgress;
 
-		shotCooldownProgress = 0;
-		Instantiate(pewPewPrefab, pewPewHolder);
-		if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out var hit, range))
-		{
-			var target = hit.transform.GetComponent<Target>();
-			if (target != null)
-			{
-				target.TakeDamage(damage);
-				if (!target.IsDead) Debug.Log($"{hit.transform.name} took 10 damage");
-			}
-		}
-	}
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        shotCooldownProgress += Time.deltaTime * shotsPerSecond;
+        if (inputs.shoot) Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (shotCooldownProgress < 1f) return;
+
+        shotCooldownProgress = 0;
+        Instantiate(pewPewPrefab, pewPewHolder);
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out var hit, range))
+        {
+            shootingSound.Play();
+
+            var target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+                if (!target.IsDead) Debug.Log($"{hit.transform.name} took 10 damage");
+            }
+        }
+    }
 }
